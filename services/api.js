e0a293b4f70d66f16b7f91099dd9a96cba51a07e
Dark <kilometro0ec@gmail.com>
2026-04-29 03:15:11 -0500

    Update api.js

--- services/api.js
+++ services/api.js
@@ -1,6 +1,6 @@
-const API = window.APP_CONFIG.API_URL;
+const API_URL = "https://script.google.com/macros/s/AKfycbwglNauq2RBBZqcPkamIBqioTShRzO_Jo9Afjo7r0OWC7AKy6740QEeOywZ26Pssm6u/exec";
 
 export async function get(sheet) {
-  const res = await fetch(`${API}?pestaña=${sheet}`);
-  return res.json();
+  const res = await fetch(`${API_URL}?pestaña=${sheet}`);
+  return await res.json();
 }
