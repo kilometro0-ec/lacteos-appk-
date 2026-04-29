5dc82d831754554fddb49437420e9bc1bb263fd7
Dark <kilometro0ec@gmail.com>
2026-04-29 02:20:39 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,9 @@
+export const CONFIG = {
+  APP_NAME: "Lógica Láctea",
+  EMPRESA: "Kilómetro Cero",
+  VERSION: "1.0.0",
 
+  API_URL: "https://script.google.com/macros/s/AKfycbwglNauq2RBBZqcPkamIBqioTShRzO_Jo9Afjo7r0OWC7AKy6740QEeOywZ26Pssm6u/exec",
+
+  CIUDAD: "Quito"
+};
