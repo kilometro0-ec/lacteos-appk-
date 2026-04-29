51d7f7cb0adb3ffb04db2754d210ea526cc26300
Dark <kilometro0ec@gmail.com>
2026-04-29 02:49:53 -0500

    Refactor config to use window.APP_CONFIG
    Changed export to window.APP_CONFIG and removed unused properties.

--- config.js
+++ config.js
@@ -1,9 +1,4 @@
-export const CONFIG = {
-  APP_NAME: "Lógica Láctea",
-  EMPRESA: "Kilómetro Cero",
-  VERSION: "1.0.0",
-
+window.APP_CONFIG = {
   API_URL: "https://script.google.com/macros/s/AKfycbwglNauq2RBBZqcPkamIBqioTShRzO_Jo9Afjo7r0OWC7AKy6740QEeOywZ26Pssm6u/exec",
-
-  CIUDAD: "Quito"
+  APP_NAME: "Lógica Láctea"
 };
