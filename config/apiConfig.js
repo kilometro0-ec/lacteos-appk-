473163a01c843dea01a01bc9306290eef7bb944e
Dark <kilometro0ec@gmail.com>
2026-04-29 00:50:38 -0500

    Update apiConfig.js

--- config/apiConfig.js
+++ config/apiConfig.js
@@ -1,3 +1,3 @@
 export const API = {
-  BASE_URL: "https://script.google.com/macros/s/TU_API/exec"
+  BASE_URL: "https://script.google.com/macros/s/AKfycby68_4A0MEp7EDNa89HGePvhWC56j3GyKx8RRIX3BGd-E_J6DG3sT-8DEXEYMXr_qMTrA/exec"
 };
